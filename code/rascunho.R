bf6a441bd6da9f8512f845b09b7e6303ade0635e
luizfcp <luizfcpassos@hotmail.com>
2019-03-31 19:06:45 -0300

    script r

---
+++ code/rascunho.R
@@ -0,0 +1,22 @@
+# Coleta feita no dia 31/03/2019
+
+library(dplyr)
+library(lexiconPT)
+library(stringr)
+library(tidytext)
+library(stopwords)
+library(magrittr)
+
+base_dilma <- read.csv2("dilma.csv", encoding = "UTF-8") %>% as_tibble()
+
+base_dilma %<>% 
+  mutate(
+    tweet = as.character(tweet) %>% str_replace_all("(://|/)", ""),
+    line = 1:nrow(.)
+    ) %>% 
+  select(line, tweet) %>% 
+  unnest_tokens(word, tweet) 
+  
+my_stop_words <- tibble(word = c(stopwords("pt"), base_dilma$word[grep("^http", base_dilma$word)]))
+
+base_dilma %<>% anti_join(my_stop_words)
