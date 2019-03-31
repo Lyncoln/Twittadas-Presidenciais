ad22aea11f1b429d79f552072beda38b69373a5d
Daniel dos Santos <dd_santos@id.uff.br>
2019-03-31 18:52:45 -0300

    Number of tweets to 20k, Folder creation

--- coletor_data.py
+++ coletor_data.py
@@ -1,7 +1,9 @@
+import os
+
 import tweepy
 import pandas as pd
 
-consumer_key = ""
+consumer_key = ''
 consumer_secret = ''
 access_token = ''
 access_token_secret = ''
@@ -19,16 +21,18 @@ presidents_users = {'Dilma Rousseff': 'dilmabr',
                     'Fernando Collor de Mello' : 'Collor'                    
                     }
 
+os.mkdirs('../data/csv')
+
 for president_user in presidents_users.keys():
     print(president_user)
 
     all_tweets = []
     user = presidents_users[president_user]
-    new_tweets = api.user_timeline(user, tweet_mode='extended', count=10)
+    new_tweets = api.user_timeline(user, tweet_mode='extended', count=20000)
     all_tweets.extend(new_tweets)
 
-    all_tweets = [[tweet.full_text.replace('\n',' '),tweet.retweet_count,tweet.favorite_count] for tweet in all_tweets]
+    all_tweets = [[tweet.full_text.replace('\n',' '), tweet.retweet_count, tweet.favorite_count] for tweet in all_tweets]
 
     tweets_df = pd.DataFrame(all_tweets)
     tweets_df.columns = ['tweet','retweet','favoritos']
-    tweets_df.to_csv(president_user + '.csv', sep=';', encoding='utf-8')
+    tweets_df.to_csv('../data/csv' + president_user + '.csv', sep=';', encoding='utf-8')
