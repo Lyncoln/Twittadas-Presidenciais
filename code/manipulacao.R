435f7dfab2d5d9a17e40ae2b86f2db25b067bc08
luizfcp <luizfcpassos@hotmail.com>
2019-04-06 23:34:41 -0300

    rm csv fhc

--- code/manipulacao.R
+++ code/manipulacao.R
@@ -1,11 +1,10 @@
 
-# Coleta feita no dia 03/04/2019
+# Coleta feita no dia 03/04/2019 via API do twitter pelo python
 
 # Pacotes utilizados ------------------------------------------------------
 
 library(dplyr)
 library(stringr)
-# library(readr)
 library(purrr)
 library(magrittr)
 library(tidytext)
@@ -13,7 +12,7 @@ library(lexiconPT)
 library(stopwords)
 library(tidyr)
 library(ggplot2)
-library(ggsci)
+library(ggwordcloud)
 
 
 # Leitura dos dados -------------------------------------------------------
@@ -29,6 +28,14 @@ base <-
 
 # Tidy Text ---------------------------------------------------------------
 
+# STOPWORDS
+
+sw <- c(
+  stopwords("pt"), 
+  "",",","rt","é","q","p","c","link","en","r","ai","h","a","ñ","ª","º","pq","el","la","d"
+)
+
+
 base %<>% 
   mutate(
     tidytext = map(
@@ -41,93 +48,221 @@ base %<>%
         select(line, tweet) %>% 
         unnest_tokens(word, tweet) %>% 
         mutate(word = str_remove_all(word, "[:number:]{1,}")) %>% 
-        anti_join(.,
-                  tibble(
-                    word = c(stopwords("pt"), 
-                             .$word[grep("^http", .$word)],
-                             "",",","rt","é","q","p","c","link","en","r","ai","h","a","ñ")
-                  )
-        ) 
+        anti_join(., tibble(word = c(sw, .$word[grep("^http", .$word)]))) 
     )
   )
-  
+
+
+{
+  base$tidytext[[1]] %<>% filter(!word %in% c("dilma", "dilmabr"))                 # Dilma
+  base$tidytext[[2]] %<>% filter(!word %in% c("collor"))                           # Collor
+  base$tidytext[[3]] %<>% filter(!word %in% c("itamar", "franco", "itamarfranco")) #Itamar
+  base$tidytext[[4]] %<>% filter(!word %in% c("jair", "bolsonaro"))                #Bolsonaro
+  base$tidytext[[5]] %<>% filter(!word %in% c("lula", "lulapresidente"))           #Lula
+  base$tidytext[[6]] %<>% filter(!word %in% c("michel", "temer"))                  #Temer
+}
 
 # Word Cloud --------------------------------------------------------------
 
-library(ggwordcloud)
-library(wordcloud)
-library(RColorBrewer)
-
-
-# Estilo 1
-
-# map(
-#   base$tidytext,
-#   ~ base$tidytext[[1]] %>%
-#     select(word) %>%
-#     count(word, sort = T) %>%
-#     with(wordcloud(word, n, max.words = 150, min.freq = 5, colors = brewer.pal(8, "Dark2")))
-# )
-
-
-# Estilo 2
 
 base %<>% 
   mutate(
-    wordcloud = map(
-      tidytext,
+    wordcloud = map2(
+      tidytext, presidente,
       ~ .x %>% 
         select(word) %>%
         count(word, sort = T) %>%
         head(150) %>%
-        # mutate(angle = 45 * sample(-2:2, n(), replace = TRUE, prob = c(1, 1, 4, 1, 1))) %>%
         ggplot(aes(
           label = word, size = n, color = factor(n)
         )) +
         scale_size_area(max_size = 7) +
-        geom_text_wordcloud()
+        geom_text_wordcloud() + 
+        theme_minimal() +
+        ggtitle(paste0("Word Cloud - ", .y)) +
+        theme(plot.title = element_text(size = 8))
     )
   )
 
+
+# Salvando
+walk2(base$wordcloud, base$presidente,
+      ~ ggsave(
+        paste0("../man/img/wordcloud/", .y, ".png"), 
+        plot = .x, dpi = "retina", width = 6.53, height = 3.11
+      )
+)
 
 # 10 palavras mais frequentes ---------------------------------------------
 
+
 base %<>% 
   mutate(
-    freq_word = map(
-      tidytext,
+    barras = map2(
+      tidytext, base$presidente,
       ~ .x %>% 
         select(word) %>% 
         count(word, sort = T) %>% 
-        head(10) %>% 
-        mutate(word = str_to_title(word))
-    )
-  )
-
-
-# Grafico
-
-base %<>% 
-  mutate(
-    grafico = map2(
-      freq_word, base$presidente,
-      ~ .x %>% 
-        ggplot(aes(x = reorder(word, n), y = n, fill = word)) +
+        head(20) %>% 
+        mutate(word = str_to_title(word)) %>% 
+        
+        ggplot(aes(x = reorder(word, n), y = n, fill = n)) +
         geom_bar(stat = 'identity') +
         coord_flip() +
-        labs(x = "", y = "Frequência", title = paste0("Palavras mais frequentes - ", .y),
-             caption = "Fonte: Twitter pessoal do presidente") +
+        labs(x = "", y = "Frequência", title = paste0("As 20 palavras mais frequentes - ", .y),
+             caption = "Fonte: Twitter pessoal") +
         theme_minimal() +
-        scale_fill_rickandmorty() +
         guides(fill = FALSE) +
-        geom_text(aes(label = n), hjust = -0.2)
+        geom_text(aes(label = n), hjust = 1.3, color = "white", fontface = "bold")
     )
   )
+
+
+# Salvando
+walk2(base$barras, base$presidente,
+  ~ ggsave(
+    paste0("../man/img/bar chart/", .y, ".png"), 
+    plot = .x, dpi = "retina", width = 12.38, height = 6.22    
+    )
+)
+
+
+# # - -----------------------------------------------------------------------
+# 
+# library(igraph)
+# library(ggraph)
+# 
+# dilma_bigram <- base$data[[1]] %>% 
+#   mutate(
+#     tweet = as.character(tweet) %>% str_replace_all("(://|/)", ""),
+#     line = 1:nrow(.)
+#   ) %>% 
+#   select(line, tweet) %>% 
+#   unnest_tokens(bigram, tweet, token = "ngrams", n = 2) %>% 
+#   separate(bigram, c("word1", "word2"), sep = " ") %>% 
+#   mutate(
+#     word1 = str_remove_all(word1, "[:number:]{1,}"),
+#     word2 = str_remove_all(word2, "[:number:]{1,}")
+#     ) %>% 
+#   filter(!word1 %in% sw) %>%
+#   filter(!word2 %in% sw) %>% 
+#   count(word1, word2, sort = TRUE)
+# 
+# dilma_bigram_graph <- 
+#   dilma_bigram %>% 
+#   filter(n > 10) %>%
+#   graph_from_data_frame()
+# 
+# set.seed(2019)
+# 
+# ggraph(dilma_bigram_graph, layout = "fr") +
+#   geom_edge_link() +
+#   geom_node_point() +
+#   geom_node_text(aes(label = name), vjust = 1, hjust = 1)
 
 
 # Analise de Sentimento ---------------------------------------------------
 
+data("oplexicon_v3.0")
+data("sentiLex_lem_PT02")
 
+op30 <- oplexicon_v3.0
+sent <- sentiLex_lem_PT02
+
+# dilma_unnest <- base$tidytext[[1]]
+# 
+# dilma_sentiment <- 
+#   dilma_unnest %>% 
+#   filter(!word %in% "temer") %>% 
+#   inner_join(op30, by = c("word" = "term")) %>% 
+#   inner_join(sent %>% select(term, lex_polarity = polarity), by = c("word" = "term")) %>% 
+#   group_by(line) %>% 
+#   summarise(
+#     tweet_sentiment_op = sum(polarity),
+#     tweet_sentiment_lex = sum(lex_polarity),
+#     n_words = n()
+#   ) %>% 
+#   ungroup() %>% 
+#   rowwise() %>% 
+#   mutate(
+#     most_neg = min(tweet_sentiment_lex, tweet_sentiment_op),
+#     most_pos = max(tweet_sentiment_lex, tweet_sentiment_op)
+#   ) %>% 
+#   filter(n_words > 2)
+
+# dilma_sentiment %>% arrange(n_words %>% desc)
+# 
+# dilma_sentiment %>% 
+#   ggplot(aes(x = tweet_sentiment_op, y = tweet_sentiment_lex)) +
+#   geom_point(aes(color = n_words)) + 
+#   scale_color_continuous(low = "green", high = "red") +
+#   labs(x = "Polaridade no OpLexicon", y = "Polaridade no SentiLex") +
+#   #geom_smooth(method = "lm") +
+#   geom_vline(xintercept = 0, linetype = "dashed") +
+#   geom_hline(yintercept = 0, linetype = "dashed")
+
+# most_pos <- which.max(dilma_sentiment$most_pos)
+# 
+# cat(base$data[[1]]$tweet[(base$data[[1]]$X +1) == dilma_sentiment$line[most_pos]])
+# 
+# 
+# base$data[[1]] %>% mutate(
+#   tweet = as.character(tweet) %>% str_replace_all("(://|/)", "")
+# ) %>% anti_join(., tibble(tweet = c(.$tweet[grep("^http", .$tweet)]))) %>% 
+#   .[116,] %$% tweet
+
+
+
+
+base %<>% 
+  mutate(
+    sentiment = map(
+      tidytext,
+      ~ .x %>%  
+        filter(!word %in% "temer") %>% 
+        inner_join(op30, by = c("word" = "term")) %>% 
+        inner_join(sent %>% select(term, lex_polarity = polarity), by = c("word" = "term")) 
+    )
+  )
+
+
+base$sentiment %<>% 
+  map(
+    ~ .x %>% 
+      group_by(line) %>% 
+      summarise(
+        tweet_sentiment_op = sum(polarity),
+        tweet_sentiment_lex = sum(lex_polarity),
+        n_words = n()
+      ) %>% 
+      ungroup() %>% 
+      rowwise() %>% 
+      mutate(
+        most_neg = min(tweet_sentiment_lex, tweet_sentiment_op),
+        most_pos = max(tweet_sentiment_lex, tweet_sentiment_op)
+      ) %>% 
+      filter(n_words > 2)
+  )
+
+# Tweet mais positivo
+
+most_pos <- which.max(dilma_sentiment$most_pos)
+
+base %>% 
+  mutate(
+    tweet_pos = pmap(
+      list(x = data, y = sentiment),
+      function(x) {
+        most_pos <- which.max(dilma_sentiment$most_pos)
+      }
+    )
+  )
+
+
+base$data[[1]] %>% mutate(
+  tweet = as.character(tweet) %>% str_replace_all("(://|/)", "")
+) %>% anti_join(., tibble(tweet = c(.$tweet[grep("^http", .$tweet)]))) %>%
+  .[116,] %$% tweet
 
 
 
